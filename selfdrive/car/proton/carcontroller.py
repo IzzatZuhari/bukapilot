b0ddb4acd8a265c14dad3f61fb84bdacd3d89ccc
IzzatZuhari <izzatzuhari@gmail.com>
2024-11-09 11:55:39 +0800

    Update carcontroller.py

--- selfdrive/car/proton/carcontroller.py
+++ selfdrive/car/proton/carcontroller.py
@@ -12,120 +12,75 @@ import cereal.messaging as messaging
 from common.features import Features
 
 RES_INTERVAL = 500
-RES_LEN = 2 # Press resume for 2 frames
+RES_LEN = 2  # Press resume for 2 frames
 
-def apply_proton_steer_torque_limits(apply_torque, apply_torque_last, driver_torque, LIMITS):
+def apply_proton_steer_torque_limits(apply_torque, apply_torque_last, driver_torque, limits):
+    """
+    Limits steer torque based on driver torque and set values.
 
-  # limits due to driver torque
-  driver_max_torque = LIMITS.STEER_MAX + driver_torque * 30
-  driver_min_torque = -LIMITS.STEER_MAX + driver_torque * 30
-  max_steer_allowed = max(min(LIMITS.STEER_MAX, driver_max_torque), 0)
-  min_steer_allowed = min(max(-LIMITS.STEER_MAX, driver_min_torque), 0)
-  apply_torque = clip(apply_torque, min_steer_allowed, max_steer_allowed)
+    Args:
+        apply_torque: Desired torque from control system.
+        apply_torque_last: Last applied torque.
+        driver_torque: Estimated driver torque input.
+        limits: Car specific limits for steer torque.
 
-  # slow rate if steer torque increases in magnitude
-  if apply_torque_last > 0:
-    apply_torque = clip(apply_torque, max(apply_torque_last - LIMITS.STEER_DELTA_DOWN, -LIMITS.STEER_DELTA_UP),
-                        apply_torque_last + LIMITS.STEER_DELTA_UP)
-  else:
-    apply_torque = clip(apply_torque, apply_torque_last - LIMITS.STEER_DELTA_UP,
-                        min(apply_torque_last + LIMITS.STEER_DELTA_DOWN, LIMITS.STEER_DELTA_UP))
+    Returns:
+        Limited steer torque value.
+    """
 
-  return int(round(float(apply_torque)))
+    driver_max_torque = limits.STEER_MAX + driver_torque * 25.5
+    driver_min_torque = -limits.STEER_MAX + driver_torque * 25.5
+    max_steer_allowed = max(min(limits.STEER_MAX, driver_max_torque), 0)
+    min_steer_allowed = min(max(-limits.STEER_MAX, driver_min_torque), 0)
+    apply_torque = clip(apply_torque, min_steer_allowed, max_steer_allowed)
 
-class CarControllerParams():
-  def __init__(self, CP):
+    # Slower rate increase for higher torque changes
+    if apply_torque_last > 0:
+        delta_up = limits.STEER_DELTA_UP
+        delta_down = limits.STEER_DELTA_DOWN / 2.0  # Slower decrease
+    else:
+        delta_up = limits.STEER_DELTA_UP / 2.0  # Slower increase
+        delta_down = limits.STEER_DELTA_DOWN
 
-    self.STEER_MAX = CP.lateralParams.torqueV[0]
-    # make sure Proton only has one max steer torque value
-    assert(len(CP.lateralParams.torqueV) == 1)
+    apply_torque = clip(apply_torque, max(apply_torque_last - delta_down, -delta_up),
+                       apply_torque_last + delta_up)
 
-    # for torque limit calculation
-    self.STEER_DELTA_UP = 20                      # torque increase per refresh, 0.8s to max
-    self.STEER_DELTA_DOWN = 30                    # torque decrease per refresh
+    return int(round(float(apply_torque)))
 
-class CarController():
-  def __init__(self, dbc_name, CP, VM):
-    self.last_steer = 0
-    self.steer_rate_limited = False
-    self.steering_direction = False
-    self.params = CarControllerParams(CP)
-    self.packer = CANPacker(DBC[CP.carFingerprint]['pt'])
-    self.disable_radar = Params().get_bool("DisableRadar")
-    self.num_cruise_btn_sent = 0
-    self.temp_lead_dist = 0       # The last lead distance before standstill
-    self.last_res_press_frame = 0 # The frame where the last resume press was finished
-    self.resume_counter = 0       # Counter for tracking the progress of a resume press
 
-    f = Features()
-    self.mads = f.has("StockAcc")
+class CarControllerParams:
+    """
+    Stores car specific parameters for the CarController.
+    """
+    def __init__(self, CP):
+        self.STEER_MAX = CP.lateralParams.torqueV[0]
+        assert len(CP.lateralParams.torqueV) == 1, "Proton only has one max steer torque value"
 
-  def update(self, enabled, CS, frame, actuators, lead_visible, rlane_visible, llane_visible, pcm_cancel, ldw, laneActive):
-    can_sends = []
-    lat_active = enabled and laneActive
-    # tester present - w/ no response (keeps radar disabled)
-    if CS.CP.openpilotLongitudinalControl and self.disable_radar:
-      if (frame % 10) == 0:
-        can_sends.append([0x7D0, 0, b"\x02\x3E\x80\x00\x00\x00\x00\x00", 0])
+        # Limits for steer torque rate changes
+        self.STEER_DELTA_UP = 20  # Torque increase per refresh
+        self.STEER_DELTA_DOWN = 30  # Torque decrease per refresh
 
-    if frame <= 1000 and CS.out.cruiseState.available and self.num_cruise_btn_sent <= 5:
-      self.num_cruise_btn_sent += 1
-      can_sends.append(send_buttons(self.packer, frame % 16, True))
 
-    # steer
-    new_steer = int(round(actuators.steer * self.params.STEER_MAX))
+class CarController:
+    def __init__(self, dbc_name, CP, VM):
+        self.last_steer = 0
+        self.steer_rate_limited = False
+        self.steering_direction = False
+        self.params = CarControllerParams(CP)
+        self.packer = CANPacker(DBC[CP.carFingerprint]['pt'])
+        self.disable_radar = Params().get_bool("DisableRadar")
+        self.num_cruise_btn_sent = 0
+        self.temp_lead_dist = 0  # The last lead distance before standstill
+        self.last_res_press_frame = 0  # The frame where the last resume press was finished
+        self.resume_counter = 0  # Counter for tracking the progress of a resume press
 
-    # stock Lane Departure Prevention (blue line)
-    # Code was fixed, commented because it could be dangerous without working with stock LKA at the same time.
-    '''
-    if not lat_active and (CS.lks_aux and CS.lks_tactile) and \
-        (CS.stock_ldp_left or CS.stock_ldp_right) and CS.stock_ldp_cmd != 0:
-      steer_dir = 1 if CS.stock_ldp_right else -1
-      new_steer = CS.stock_ldp_cmd * steer_dir * 0.5 # Reduced value because stock command was strong
-      lat_active = True
-    '''
+        self.mads = Features().has("StockAcc")  # Check for MADS support
 
-    apply_steer = apply_proton_steer_torque_limits(new_steer, self.last_steer, 0, self.params)
-    self.steer_rate_limited = (new_steer != apply_steer) and (apply_steer != 0)
+    def update(self, enabled, CS, frame, actuators, lead_visible, rlane_visible, llane_visible, pcm_cancel, ldw, laneActive):
+        can_sends = []
+        lat_active = enabled and laneActive
 
-    ts = frame * DT_CTRL
-    self.is_alc_enabled = Params().get_bool("IsAlcEnabled")
-
-    # CAN controlled lateral running at 50hz
-    if (frame % 2) == 0:
-      can_sends.append(create_can_steer_command(self.packer, apply_steer, lat_active, \
-          CS.hand_on_wheel_warning and CS.is_icc_on, (frame/2) % 16, \
-          CS.lks_aux, CS.lks_audio, CS.lks_tactile, CS.lks_enable_main, CS.stock_ldw))
-
-      #can_sends.append(create_hud(self.packer, apply_steer, enabled, ldw, rlane_visible, llane_visible))
-      #can_sends.append(create_lead_detect(self.packer, lead_visible, enabled))
-      #if CS.out.genericToggle:
-      #  fake_enable = True
-      #else:
-      #  fake_enable = False
-      #can_sends.append(create_acc_cmd(self.packer, actuators.accel, fake_enable, (frame/2) % 16))
-
-    # For resume
-    if CS.out.standstill and enabled and self.resume_counter == 0 and \
-        frame > (self.last_res_press_frame + RES_INTERVAL) and CS.leadDistance > self.temp_lead_dist:
-      # Only start a new resume if the last one was finished, with an interval
-      self.resume_counter = 1 # Start a new resume press
-    elif not enabled or (enabled and not CS.out.standstill):
-      # cruise control not enabled or moving with cruise control, record the distance
-      self.temp_lead_dist = CS.leadDistance
-
-    if self.resume_counter > 0 and self.resume_counter <= RES_LEN and CS.out.standstill:
-      if not self.mads or CS.acc_req: # Send resume press signal
-          can_sends.append(send_buttons(self.packer, frame % 16, False))
-      self.resume_counter += 1
-
-    if self.resume_counter > RES_LEN or not CS.out.standstill:
-       # If resume press is finished or car is moving
-       self.last_res_press_frame = frame # Store the frame where last resume press was finished
-       self.resume_counter = 0 # Reset resume counter
-
-    self.last_steer = apply_steer
-    new_actuators = actuators.copy()
-    new_actuators.steer = apply_steer / self.params.STEER_MAX
-
-    return new_actuators, can_sends
+        # Disable radar with tester present (no response)
+        if CS.CP.openpilotLongitudinalControl and self.disable_radar:
+            if frame % 10 == 0:
+                can_sends.append([0x7D0, 0
